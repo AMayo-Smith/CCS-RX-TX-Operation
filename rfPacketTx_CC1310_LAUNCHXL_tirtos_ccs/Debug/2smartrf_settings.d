ddf86cc95413cd1fbc520f621a4225a4a19e59e5
AMayo-Smith <56044642+AMayo-Smith@users.noreply.github.com>
2020-03-06 00:08:43 -0500

    Add files via upload

---
+++ rfPacketTx_CC1310_LAUNCHXL_tirtos_ccs/Debug/2smartrf_settings.d
@@ -0,0 +1,57 @@
+# FIXED
+
+2smartrf_settings.obj: ../2smartrf_settings.c
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/DeviceFamily.h
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/driverlib/rf_mailbox.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/string.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_ti_config.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/linkage.h
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/driverlib/rf_common_cmd.h
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/driverlib/rf_prop_cmd.h
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/drivers/rf/RF.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdbool.h
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/drivers/dpl/ClockP.h
+2smartrf_settings.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stddef.h
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/drivers/dpl/SemaphoreP.h
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/drivers/utils/List.h
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/driverlib/rf_ble_cmd.h
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/rf_patches/rf_patch_cpe_genfsk.h
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/rf_patches/rf_patch_rfe_genfsk.h
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/rf_patches/../inc/hw_types.h
+2smartrf_settings.obj: C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/rf_patches/../inc/../inc/hw_chip_def.h
+
+../2smartrf_settings.c: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/DeviceFamily.h: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/driverlib/rf_mailbox.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/string.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_ti_config.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/linkage.h: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/driverlib/rf_common_cmd.h: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/driverlib/rf_prop_cmd.h: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/drivers/rf/RF.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdbool.h: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/drivers/dpl/ClockP.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stddef.h: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/drivers/dpl/SemaphoreP.h: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/drivers/utils/List.h: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/driverlib/rf_ble_cmd.h: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/rf_patches/rf_patch_cpe_genfsk.h: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/rf_patches/rf_patch_rfe_genfsk.h: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/rf_patches/../inc/hw_types.h: 
+C:/ti/simplelink_cc13x0_sdk_3_20_00_23/source/ti/devices/cc13x0/rf_patches/../inc/../inc/hw_chip_def.h: 
